110f427728216f22a5cdf7018e233a1b84fa10b0
Robert Lin <Rlin415@gmail.com>
2016-06-22 10:44:53 -0700

    completed tests for total_infection

--- virus_spec/index.js
+++ virus_spec/index.js
@@ -13,6 +13,41 @@ describe('Virus', function() {
 
   describe('#totalInfection', function() {
 
+    let outside, inside, max, begUser;
+
+    before(function() {
+      inside = [];
+      outside = [];
+      max = 20;
+      begUser = User.create('A', 1);
+
+      inside.push(begUser);
+
+      for (let i = 2; i < 20; i++) {
+        let user = User.create('A', i);
+        for (let j = max; j < max + i; j++) {
+          if (Math.floor(Math.random() * 2)) {
+            let teacher = User.create('A', j);
+            user.addTeacher(teacher);
+            inside.push(teacher);
+          } else {
+            let student = User.create('A', j);
+            user.addStudent(student);
+            inside.push(student);
+          }
+        }
+        max = max + i;
+        inside.push(user);
+        Math.floor(Math.random() * 2) ? begUser.addTeacher(user) : begUser.addStudent(user);
+      }
+
+      for (let i = max; i < max + 100; i++) {
+        let user = User.create('A', i);
+        outside.push(user);
+      }
+
+    });
+
     it('should be a function', function() {
       expect(virus.totalInfection).to.be.a('function');
     });
@@ -35,11 +70,22 @@ describe('Virus', function() {
     });
 
     it('should infect all users that are inside the coaching graph of an infected user', function() {
-
+      virus.totalInfection(begUser, 'B');
+      expect(inside).to.satisfy(function(users) {
+        for (let user of users) {
+          if (user.version !== 'B') return false;
+        }
+        return true;
+      });
     });
 
     it('should not infect any users that are outside the coaching graph of an infected user', function() {
-
+      expect(outside).to.satisfy(function(users) {
+        for (let user of users) {
+          if (user.version === 'B') return false;
+        }
+        return true;
+      });
     });
 
   });
